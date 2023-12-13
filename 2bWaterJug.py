ed35808463e608f8c899e08184cd69c3c9d15b0b
Zara0911 <153439762+Zara0911@users.noreply.github.com>
2023-12-13 22:24:02 +0530

    Update 2bWaterJug.py

--- 2bWaterJug.py
+++ 2bWaterJug.py
@@ -18,3 +18,18 @@ def pour (jug1, jug2):
 
 print("JUG1\tJUG2")
 pour(0,0)
+
+
+
+
+
+
+'''Water jug problem:
+The water jug problem is a classic puzzle that involves two or three jugs and the task
+of measuring a specific quantity of water using those jugs.
+The constraints of the problem are as follows:
+I. You can fill the jugs completely from the water source.
+II. You can empty the jugs completely onto the ground.
+III. You can transfer water from one jug to another until the source jug is empty
+or the target jug is full.'''
+
