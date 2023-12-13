713f22b63826a68702e74f0845e86d4eddd8f118
Zara0911 <153439762+Zara0911@users.noreply.github.com>
2023-12-13 22:25:54 +0530

    Update 4aAstarandAOstar.py

--- 4aAstarandAOstar.py
+++ 4aAstarandAOstar.py
@@ -29,3 +29,17 @@ print(result.path())
 CMD COMMAND
 1.pip install simpleai
 2.pip install pydot flask
+
+
+
+
+'''Explanation:
+The A* (pronounced "A-star") algorithm is a popular and widely used path finding
+algorithm in computer science and artificial intelligence. It is primarily used to find
+the shortest path between two points on a graph or grid while efficiently exploring
+the most promising nodes along the way. The A* algorithm combines the advantages
+of a heuristic search.
+The AO* algorithm is based on AND-OR graphs to break complex problems into
+smaller ones and then solve them. The AND side of the graph represents those tasks
+that need to be done with each other to reach the goal, while the OR side stands
+alone for a single task'''
