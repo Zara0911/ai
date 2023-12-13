f4c852e3e563e117d7b4998b23cbbb83e8abaa0b
Zara0911 <153439762+Zara0911@users.noreply.github.com>
2023-12-13 22:23:06 +0530

    Update 2aTowerofHanoi.py

--- 2aTowerofHanoi.py
+++ 2aTowerofHanoi.py
@@ -8,3 +8,9 @@ def moveDisk(fp, tp):
     print("Moving Disk from", fp, "to", tp) 
  
 moveTower(3, "A", "B", "C") 
+
+
+'''Tower of Hanoi:
+The Tower of Hanoi is a mathematical puzzle that consists of three rods and a number
+of disks of different sizes that can slide onto any rod. The objective of the puzzle is to
+move the entire stack of disks from one rod to another'''
