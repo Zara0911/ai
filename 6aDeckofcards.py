f34d4ca2e5eb8ccfc380bac922105f4acd74a887
Zara0911 <153439762+Zara0911@users.noreply.github.com>
2023-12-13 20:42:58 +0530

    Update 6aDeckofcards.py

--- 6aDeckofcards.py
+++ 6aDeckofcards.py
@@ -4,3 +4,20 @@ random.shuffle(deck)
 print("You got:")
 for i in range(5):
       print(deck[i][0], "of", deck[i][1])
+
+
+///////////////////////////////////////////////////////////////
+
+beforeshuffel and aftershuffel.
+import random 
+s=["ace",1,2,3,4,5,6,7,8,9,"king","joker","queen"] 
+h=["ace",1,2,3,4,5,6,7,8,9,"king","joker","queen"] 
+l=["ace",1,2,3,4,5,6,7,8,9,"king","joker","queen"] 
+d=["ace",1,2,3,4,5,6,7,8,9,"king","joker","queen"] 
+print("\nBefore shuffle\n") print(s) print(h) 
+print(l) print(d) print("\nAfter shuffle\n") 
+shuf=random.shuffle(s) print(s) 
+shuf=random.shuffle(h) print(h) 
+shuf=random.shuffle(l) 
+print(l) 
+shuf=random.shuffle(d) print(d)
